2d4873d75a50d74a6b7cbac7847cc28d128030fc
shahmehul53 <mehul.developer2019@gmail.com>
2022-08-12 01:02:54 +0530

    Api Call

--- app/src/scenes/Details.js
+++ app/src/scenes/Details.js
@@ -1,13 +1,38 @@
 import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+
+const API_KEY = '971d3aad3083a2a5c2fce97ca8006581';
 
 const Details = ({route}) => {
   const {data} = route.params;
+  const [cityName, setCityName] = useState('');
+  const [weather, setWeather] = useState("")
+
+  useEffect(() => {
+    // setCityName()
+    apiCall();
+  }, []);
+
+  const apiCall = async() => {
+   await axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${data?.item?.name}&appid=${API_KEY}`,
+      )
+      .then(response => {
+        console.log('response', response.data);
+        setWeather(response.data)
+      }).catch((err) => console.log("err", err))
+  };
+
   console.log('data', data);
   return (
     <View>
-      <Text>Details</Text>
       <Text>{data?.item?.name}</Text>
+      <Text>Temp : {weather?.main?.temp}</Text>
+      <Text>Description : {weather?.weather?.[0]?.description}</Text>
+      <Text>Humidity : {weather?.main?.humidity}</Text>
+      <Text>Pressure : {weather?.main?.pressure}</Text>
     </View>
   );
 };
